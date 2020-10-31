33856ddb4b863caf49818aed1095107887bea738
vagrant <k.taki415@gmail.com>
2020-10-31 09:47:14 +0000

    [Add] NotNull

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_20_033621) do
+ActiveRecord::Schema.define(version: 2020_10_31_093433) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
