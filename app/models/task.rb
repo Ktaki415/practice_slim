7dacf898a6938e81b112d40d8458eff9271de90e
vagrant <k.taki415@gmail.com>
2020-11-11 04:00:51 +0000

    [Finish] #20

--- app/models/task.rb
+++ app/models/task.rb
@@ -7,7 +7,7 @@ class Task < ApplicationRecord
 
   belongs_to :user
 
-  scope: :recent, -> { order(created_at: :desc) }
+  scope :recent, -> { order(created_at: :desc) }
 
   private
   def validate_name_not_including_comma
