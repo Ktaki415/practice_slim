15c11681720aa80b367541647d643488e8de887e
vagrant <k.taki415@gmail.com>
2020-11-19 07:10:10 +0000

    [Change] time_zone

--- config/application.rb
+++ config/application.rb
@@ -10,9 +10,10 @@ module PSlim
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.time_zone = 'Asia/Tokyo'
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
+    # Application configuration can go into files in config/initializßers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
   end
